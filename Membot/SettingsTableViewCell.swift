340a8a13c9caa61ef7681c82cec606ee57451a59
Alex Andrews <alexcandrews91@gmail.com>
2016-03-10 13:04:04 -0700

    Begin implementation of Settings view

---
+++ Membot/SettingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsTableViewCell.swift
+//  Membot
+//
+//  Created by Alex Andrews on 3/10/16.
+//  Copyright © 2016 Sneakywolf. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
