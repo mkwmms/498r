04bd7e25999098e458245eb6b364b5d420ae7f5a
mwilliammyers <mwilliammyers@users.noreply.github.com>
2016-03-17 14:19:50 -0600

    Resize image in DayView

--- Membot/DayCollectionViewCell.swift
+++ Membot/DayCollectionViewCell.swift
@@ -12,12 +12,17 @@ class DayCollectionViewCell: UICollectionViewCell {
 
     func configureForItem(memorable: Any) {
         
-        let targetWidth = UIScreen.mainScreen().bounds.width
-        let targetSize = CGSize(width: targetWidth, height: targetWidth)
+//        self.backgroundColor = UIColor.lightGrayColor()
+        
+        let screenSize = UIScreen.mainScreen().bounds
+        let imageTargetSize = UIScreen.mainScreen().bounds
+        let targetSize = CGSize(width: imageTargetSize.width, height: imageTargetSize.width)
         if let mem = memorable as? Memorable {
             mem.adapter.retrieveDisplayableData(mem.metadata, dimensions: self.sizeThatFits(targetSize), completion: { (image) -> Void in
                 let dayImageView: UIImageView = UIImageView(image: image as? UIImage)
-                self.addSubview(dayImageView)
+                dayImageView.frame = CGRectMake(0,0, screenSize.height * 0.9, screenSize.width * 0.9)
+                dayImageView.contentMode = .ScaleAspectFit
+                self.contentView.addSubview(dayImageView)
             })
         }
     }
--- Membot/DayCollectionViewController.swift
+++ Membot/DayCollectionViewController.swift
@@ -11,14 +11,13 @@ import UIKit
 private let reuseIdentifier = "DayCollectionCellIdentifier"
 private let dayHeaderIdentifier = "DayHeaderCollectionReusableView"
 
-
-class DayCollectionViewController: UICollectionViewController {
+class DayCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     var dayDataSource: DayCollectionViewDataSource?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.collectionView?.backgroundColor = UIColor.whiteColor()
 
         dayDataSource = DayCollectionViewDataSource(cellIdentifier: reuseIdentifier, configureBlock: { (cell, memorable) -> Void in
@@ -28,17 +27,10 @@ class DayCollectionViewController: UICollectionViewController {
                 }
             }
         })
-        
-        dayDataSource?.sortMemorablesByDay()
-        
-        collectionView!.dataSource = dayDataSource
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
-        // Register cell classes
-//        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-        // Do any additional setup after loading the view.
+        dayDataSource?.sortMemorablesByDay()
+
+        collectionView!.dataSource = dayDataSource
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,66 +38,10 @@ class DayCollectionViewController: UICollectionViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
-    // MARK: - Navigation
+    // MARK: - FlowLayout
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let cellSize = UIScreen.mainScreen().bounds.width
+        return CGSize(width: cellSize, height: cellSize)
     }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-//    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//
-//
-//    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of items
-//        return 0
-//    }
-//
-//    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! DayCollectionViewCell
-//    
-//        // Configure the cell
-//    
-//        return cell
-//    }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
-    
-    }
-    */
-
 }
